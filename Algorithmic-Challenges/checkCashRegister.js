c51c2a9de110bd78c70fb2d3f39d1a1364153c97
Renzo Barrios <w3renzo@gmail.com>
2021-02-20 14:57:54 -0300

    Hardest ALgo in the fucking world

---
+++ Algorithmic-Challenges/checkCashRegister.js
@@ -0,0 +1,104 @@
+const DENOMINATIONS = {
+  'PENNY': 1,
+  'NICKEL': 5,
+  'DIME': 10,
+  'QUARTER': 25,
+  'ONE': 100,
+  'FIVE': 500,
+  'TEN': 1000,
+  'TWENTY': 2000,
+  'ONE HUNDRED': 10000,
+};
+
+function checkCashRegister(price, cash, cid) {
+  let changeSum = cash * 100 - price * 100;
+  let changeSumCheck = changeSum;
+  let change = [];
+  let status = '';
+
+  let cidSum = 0;
+  let filterCid = cid.filter(elem => elem[1] !== 0).reverse();
+
+  filterCid.forEach(elem => {
+    let curr = elem[0];
+    let currSum = elem[1] * 100;
+    cidSum += currSum;
+    let amount = 0;
+    while (changeSum >= DENOMINATIONS[curr] && currSum > 0) {
+      amount += DENOMINATIONS[curr];
+      changeSum -= DENOMINATIONS[curr];
+      currSum -= DENOMINATIONS[curr];
+    }
+
+    if (amount !== 0) {change.push([curr], amount / 100);}
+
+  });
+
+  // if ((cidSum / 100) - (changeSumCheck / 100) === 0) {
+  //   return status = 'CLOSED';
+  //
+  // } else if ((cidSum / 100) - (changeSumCheck / 100) > 0) {
+  //   return status = 'OPEN';
+  // }
+  //
+  // else if ((changeSumCheck / 100)-(cidSum / 100)  < 0) {
+  //   return status = 'INSUFFICIENT_FUNDS';
+  // }
+
+ return ((changeSumCheck / 100)-(cidSum / 100)> (cidSum / 100));
+}
+
+console.log(checkCashRegister(19.5, 20, [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]])); //should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+
+console.log(checkCashRegister(3.26, 100, [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]])); //should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+//
+// console.log(checkCashRegister(19.5, 20, [
+//   ['PENNY', 0.01],
+//   ['NICKEL', 0],
+//   ['DIME', 0],
+//   ['QUARTER', 0],
+//   ['ONE', 0],
+//   ['FIVE', 0],
+//   ['TEN', 0],
+//   ['TWENTY', 0],
+//   ['ONE HUNDRED', 0]])); //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+//
+console.log(checkCashRegister(19.5, 20, [
+  ['PENNY', 0.01],
+  ['NICKEL', 0],
+  ['DIME', 0],
+  ['QUARTER', 0],
+  ['ONE', 1],
+  ['FIVE', 0],
+  ['TEN', 0],
+  ['TWENTY', 0],
+  ['ONE HUNDRED', 0]])); //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+//
+console.log(checkCashRegister(19.5, 20, [
+  ['PENNY', 0.5],
+  ['NICKEL', 0],
+  ['DIME', 0],
+  ['QUARTER', 0],
+  ['ONE', 0],
+  ['FIVE', 0],
+  ['TEN', 0],
+  ['TWENTY', 0],
+  ['ONE HUNDRED', 0]])); //should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
