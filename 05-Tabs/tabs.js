43c72371288d4f3930736cd66271a8d7bf26ddd6
soyrenzo <w3renzo@gmail.com>
2021-02-20 15:00:37 -0300

    Tabs

--- 05-Tabs/tabs.js
+++ 05-Tabs/tabs.js
@@ -3,18 +3,18 @@ const panels = document.querySelectorAll('[role=\'tabpanel\']');
 
 function handleClick(e) {
 
-  const {id} = e.currentTarget;
+    const {id} = e.currentTarget;
 
-  panels.forEach(panel => panel.hidden = true);
-  btns.forEach(btn => btn.setAttribute('aria-selected', false));
-  let panel = document.querySelector(`[aria-labelledby=${id}]`);
-  panel.hidden = false;
-  e.currentTarget.setAttribute('aria-selected', true);
+    panels.forEach(panel => panel.hidden = true);
+    btns.forEach(btn => btn.setAttribute('aria-selected', "false"));
+    let panel = document.querySelector(`[aria-labelledby=${id}]`);
+    panel.hidden = false;
+    e.currentTarget.setAttribute("aria-selected", true);
 }
 
 btns.forEach(btn => {
-  btn.addEventListener('click', handleClick);
-  btn.addEventListener('focus', handleClick);
+    btn.addEventListener('click', handleClick);
+    btn.addEventListener('focus', handleClick);
 });
 
 
