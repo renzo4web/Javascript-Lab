df2f0e24d74192728825979e548554e062658fcf
Renzo Barrios <w3renzo@gmail.com>
2021-02-22 22:36:40 -0300

    Update isPP.js

--- Algorithmic-Challenges/isPP.js
+++ Algorithmic-Challenges/isPP.js
@@ -1,19 +1,19 @@
-const isPP = function(n) {
-  let g = 0
+const isPP = function (n) {
+  let g = 0;
 
-  for( let i = 0 ; i <10 ; i++){
-    if( i ** 2 === n || i ** 4 === n  ){
-      g  = i
+  for (let i = 0; i < 10; i++) {
+    if (i ** 2 === n || i ** 4 === n) {
+      g = i;
     }
   }
-  if(g > 1 ){
-    return [g,2]
-  }else if(n % 2 !== 0 && n !== 1){
-    return [n,1]
-  }else{
-    return null
+  if (g > 1) {
+    return [g, 2];
+  } else if (n % 2 !== 0 && n !== 1) {
+    return [n, 1];
+  } else {
+    return null;
   }
-  return null ; // fix me
+  return null; // fix me
 };
 
 console.log(isPP(4));
@@ -21,6 +21,6 @@ console.log(isPP(8281));
 console.log(isPP(27));
 console.log(isPP(5));
 
-console.log( 5 %2 ===0 )
+console.log(5 % 2 === 0);
 
 //5 isn't a perfect power - Expected: null, instead got: [5, 1]
