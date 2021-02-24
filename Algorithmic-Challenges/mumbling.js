71e94820a89580d25dd3d555e13cef11aaf281fb
Renzo Barrios <w3renzo@gmail.com>
2021-02-23 22:26:50 -0300

    refactor mumbling

--- Algorithmic-Challenges/mumbling.js
+++ Algorithmic-Challenges/mumbling.js
@@ -1,8 +1,8 @@
 function accum(str) {
-  let g = str.split('').
-      map((x, i) => x.toLowerCase().
-          repeat(i + 1)).
-      map(t => t.replace(t[0], t[0].toUpperCase()));
+  let g = str
+    .split("")
+    .map((x, i) => x.toLowerCase().repeat(i + 1))
+    .map((t) => t.replace(t[0], t[0].toUpperCase()));
 
   return g.join("-");
 }
